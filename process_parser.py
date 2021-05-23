14fe39d3e2d4d724542d57866aea090c6afdf548
xenu_xore <door.xore@gmail.com>
2021-05-23 21:22:08 +0300

    генератор паука

--- process_parser.py
+++ process_parser.py
@@ -15,46 +15,72 @@ sys.setrecursionlimit(20000)
 # Скачать web_driver
 # https://github.com/mozilla/geckodriver/releases
 
-class Conf:
+class Config:
+
+    _options = webdriver.FirefoxOptions()
+    _user_agent = None
+    _path_dir = None
+
     def __init__(self):
-        self._options = webdriver.FirefoxOptions()
         self._profile = webdriver.FirefoxProfile()
-        self._path_dir = r'C:\Users\Xorex\PycharmProjects\2gis_parser\driver\geckodriver.exe'
-        self.user_agent = "User-agent/5.0"
         self._driver = self.web_driver()
         self.queue = queue.Queue()
-        self.close_popup = self.close_popup()
-        self.click_next_page = self.click_next_page()
-        self.select_obj_href = self.select_obj_href()
-        self.click_more_phone = self.click_more_phone()
+
+    @property
+    def path_dir(self):
+        return self._path_dir
+
+    @path_dir.setter
+    def path_dir(self, value):
+        if self._path_dir is None:
+            self._path_dir = value
 
     def web_driver(self):
         if os.path.exists(self._path_dir):
-            self._profile.set_preference("general.useragent.override", f'{self.user_agent}')
+            self._profile.set_preference("general.useragent.override", f'{self._user_agent}')
             return webdriver.Firefox(self._profile, executable_path=self._path_dir, options=self._options)
         else:
-            raise FileExistsError("Не найден web_driver.")
+            raise FileExistsError(f"Не найден web_driver на пути {self._path_dir}")
+
+
+class ParserGis(Config):
+    def __init__(self):
+        super().__init__()
+        self._url = None
+        self._close_popup = None
+        self._click_next_page = None
+        self._select_obj_href = None
+
+    @property
     def close_popup(self):
         close_popup = '//*[@id="root"]/div/div/div[3]/footer/div[2]'
-        return close_popup
+        return self._close_popup
 
+    @close_popup.setter
+    def close_popup(self, value):
+        if self._close_popup is None:
+            self._close_popup = value
+
+    @property
     def click_next_page(self):
         click_next_page = '//*[@id="root"]/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]/div[' \
                           '2]/div[1]/div/div/div[1]/div[3]/div[2]/div[2]'
-        return click_next_page
+        return self._click_next_page
 
+    @click_next_page.setter
+    def click_next_page(self, value):
+        if self._click_next_page is None:
+            self._click_next_page = value
+
+    @property
     def select_obj_href(self):
         select_obj_href = "div._1h3cgic > a._pbcct4"
-        return select_obj_href
+        return self._select_obj_href
 
-    def click_more_phone(self):
-        click_more_phone = '//*[@class="_b0ke8"]/a'
-        return click_more_phone
-
-class ParserGis(Conf):
-    def __init__(self):
-        super().__init__()
-        self._url = ''
+    @select_obj_href.setter
+    def select_obj_href(self, value):
+        if self._select_obj_href is None:
+            self._select_obj_href = value
 
     @property
     def start_url(self):
@@ -62,23 +88,13 @@ class ParserGis(Conf):
 
     @start_url.setter
     def start_url(self, value):
-        if self._url == '':
+        if self._url is None:
             self._url = value
             self.execute_parser()
 
-    @property
-    def headless(self):
-        return self._options
-
-    @headless.setter
-    def headless(self, v):
-        if self._options:
-            self._options.headless = v
-
-
     def close_popup_cookies(self):
         try:
-            self._driver.find_element_by_xpath(self.close_popup).click()
+            self._driver.find_element_by_xpath(self._close_popup).click()
         except NoSuchElementException:
             print(f"Элемента нет")
 
@@ -93,14 +109,14 @@ class ParserGis(Conf):
 
     def __parser(self):
         time.sleep(random.randint(2, 6))
-        href = self._driver.find_elements_by_css_selector(self.select_obj_href)
+        href = self._driver.find_elements_by_css_selector(self._select_obj_href)
 
         for elem in href:
             self.queue.put(elem.get_attribute('href').split("?")[0])
 
-    def parser_pages(self, next_page=None):
+    def run_parser(self, next_page=True):
         self.__parser()
-        if next_page or next_page is None:
+        if next_page:
             self.__next_pages()
         else:
             print(f"Собрано ссылок {self.queue.qsize()}")
@@ -109,26 +125,59 @@ class ParserGis(Conf):
     def __next_pages(self):
         if "2gis" in self._driver.current_url:
             try:
-                self._driver.find_element_by_xpath(self.click_next_page).click()
-                self.parser_pages()
+                self._driver.find_element_by_xpath(self._click_next_page).click()
+                self.run_parser()
             except NoSuchElementException:
-                self.parser_pages(next_page=False)
+                self.run_parser(next_page=False)
                 self._driver.close()
                 self._driver.quit()
 
+    @property
     def queue_exp(self):
         return self.queue.qsize()
 
 
-class Crawl(Conf):
+class Crawl(Config):
     def __init__(self, links, export=None):
         super().__init__()
         self.result = {}
         self.__links = links
         self.export = export
-        self.h1 = '//h1'
-        self.phones = "div._49kxlr > div._b0ke8 > a._1nped2zk"
-        self.emails = "div._49kxlr > div > a._1nped2zk"
+        self.click_more_phone = self.click_more_phone()
+        self._fetch_h1 = None
+        self._fetch_phones = None
+        self._fetch_emails = None
+
+    @property
+    def fetch_h1(self):
+        return self._fetch_h1
+
+    @property
+    def fetch_phones(self):
+        return self._fetch_phones
+
+    @property
+    def fetch_emails(self):
+        return self._fetch_emails
+
+    def click_more_phone(self):
+        click_more_phone = '//*[@class="_b0ke8"]/a'
+        return click_more_phone
+
+    @fetch_h1.setter
+    def fetch_h1(self, value):
+        if self._fetch_h1 is None:
+            self._fetch_h1 = value
+
+    @fetch_phones.setter
+    def fetch_phones(self, value):
+        if self._fetch_phones is None:
+            self._fetch_phones = value
+
+    @fetch_emails.setter
+    def fetch_emails(self, value):
+        if self._fetch_emails is None:
+            self._fetch_emails = value
 
     def collect_data(self, h1, phone, email):
         name = self._driver.find_element_by_xpath(h1).text
@@ -147,7 +196,7 @@ class Crawl(Conf):
         print([phone.get_attribute('href').split(":")[1] for phone in phones])
 
         type_json = json.dumps(self.result, ensure_ascii=False)
-        with open("2gis.json", "w", encoding="utf-8") as file:
+        with open("company.json", "w", encoding="utf-8") as file:
             file.write(type_json)
 
     @staticmethod
@@ -160,9 +209,12 @@ class Crawl(Conf):
         }
 
         print([phone.get_attribute('href').split(":")[1] for phone in phones])
-        with open("2gis.csv", "a") as file:
+        with open("company.csv", "a") as file:
             writer = csv.writer(file, delimiter=";", lineterminator="\r")
             writer.writerow((org_to_csv['name'], org_to_csv['phones'], ', '.join(org_to_csv['emails'])))
+
+    def export_db(self, h1, phones, emails):
+        pass
 
     def execute_crawler(self):
         while True:
@@ -177,13 +229,16 @@ class Crawl(Conf):
             except NoSuchElementException:
                 self._driver.get(item + 1)
 
-            h1, phones, emails = self.collect_data(self.h1, self.phones, self.emails)
+            h1, phones, emails = self.collect_data(self._fetch_h1, self._fetch_phones, self._fetch_emails)
 
             if self.export == 'json':
                 self.export_json(h1, phones, emails)
 
             elif self.export == 'csv':
                 self.export_csv(h1, phones, emails)
+            elif self.export == "db":
+                self.export_db(h1, phones, emails)
+
             else:
                 raise ValueError(f'Несуществует экспорта для {self.export}. Выбирите формат экспорта csv или json')
 
@@ -197,3 +252,51 @@ class Crawl(Conf):
     def __call__(self, *args, **kwargs):
         threading.Thread(target=self.execute_crawler(), daemon=True).start()
         self.queue.join()
+
+
+class GenSpider(ABC):
+    def __init__(self):
+        Config._path_dir = self.driver()
+        Config._options.headless = self.headless()
+        Config._user_agent = self.user_agent()
+        self.parser = ParserGis()
+        self.parser.settings = self.config_window_parser()
+        self.parser.start_url = self.start_url()
+        self.parser.run_parser()
+        self.crawler = self.__crawler()
+        self.fetch_elements = self.fetch_element()
+
+    @abstractmethod
+    def driver(self):
+        pass
+
+    @abstractmethod
+    def headless(self):
+        pass
+
+    @abstractmethod
+    def user_agent(self):
+        pass
+
+    @abstractmethod
+    def start_url(self):
+        pass
+
+    @abstractmethod
+    def config_window_parser(self):
+        pass
+
+    @abstractmethod
+    def export_data(self):
+        pass
+
+    def __crawler(self):
+        return Crawl(self.parser.queue, export=self.export_data())
+
+    @abstractmethod
+    def fetch_element(self):
+        pass
+
+    @abstractmethod
+    def crawler(self):
+        pass
---
+++ spider.py
@@ -0,0 +1,75 @@
+from process_parser import GenSpider
+
+
+class Spider(GenSpider):
+    """
+    Пример генерируемого паука
+    """
+
+    def driver(self):
+        """
+        Путь к веб-драйверу
+        :return: str
+        """
+        return r'C:\Users\Xorex\PycharmProjects\2gis_parser\driver\geckodriver.exe'
+
+    def headless(self):
+        """
+        Установка режима безголовы
+        :return: bool
+        """
+        return False
+
+    def user_agent(self):
+        """
+        Установка юзер-агента
+        :return: str
+        """
+        return "User-agent/6.0"
+
+    def start_url(self):
+        """
+        Стартовый URL
+        :return: str
+        """
+        return "https://2gis.ru/krasnodar/search/%D0%9F%D0%B0%D1%80%D0%B8%D0%BA%D0%BC%D0%B0%D1%85%D0%B5%D1%80%D1%81" \
+               "%D0%BA%D0%B8%D0%B5/rubricId/305/filters/covid_services_home%3Bgeneral_payment_type_card%3Bbound" \
+               "%3Bhas_photos%3Bhas_site%3Bbarbershop_female_haircut_starting_price%3D100%2C455 "
+
+    def config_window_parser(self):
+        """
+        Конфигуратор окна. Закрытие всяких попапов, хождение по пагинации, выбор селекта детального объекта
+        :return: property
+        """
+        self.parser.close_popup = '//*[@id="root"]/div/div/div[3]/footer/div[2]'
+        self.parser.click_next_page = '//*[@id="root"]/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div/div[' \
+                                      '2]/div[2]/div[1]/div/div/div[1]/div[3]/div[2]/div[2] '
+        self.parser.select_obj_href = "div._1h3cgic > a._pbcct4"
+
+    def export_data(self):
+        """
+        Выбор формата экспорта данных
+        :return: str
+        """
+        return 'csv'
+
+    def fetch_element(self):
+        """
+        Извлечение элементов
+        :return: property
+        """
+        self.crawler.fetch_h1 = '//h1'
+        self.crawler.fetch_phones = "div._49kxlr > div._b0ke8 > a._1nped2zk"
+        self.crawler.fetch_emails = "div._49kxlr > div > a._1nped2zk"
+
+    def crawler(self):
+        """
+        Обязательно! Вызов краулера
+        :return: object class Crawl __call__
+        """
+        return self.crawler()
+
+
+if __name__ == "__main__":
+    d = Spider()
+    d.crawler()
